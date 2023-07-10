244270ff4a25c5b8b41adec2ebadec535ccb8261
mrkteq <markanthonytucker@gmail.com>
2023-07-10 21:47:05 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const todoList = document.getElementById('todoList');
+const todoInput = document.getElementById('todoInput');
+const todoButton = document.getElementById('todoButton');
+let activeTodoItem = null;
+
+function addTodoItem() {
+    if (activeTodoItem) {
+
+    }else{
+        
+    }
+}
