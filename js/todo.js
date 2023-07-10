5c6b41483303ff082c0c7bf8c774ff2d321cd5db
mrkteq <markanthonytucker@gmail.com>
2023-07-10 23:45:46 +0200

    fully working example

---
+++ js/todo.js
@@ -0,0 +1,37 @@
+(function() {
+    const todoList = document.getElementById("todoList"); 
+    const todoInput = document.getElementById("todoInput");
+    const addButton = document.getElementById("addButton");
+    let activeTodoItem = null;
+
+    function addTodo() {
+      if (activeTodoItem) {
+        activeTodoItem.firstChild.textContent = todoInput.value; 
+        activeTodoItem = null; 
+      } else { 
+        const newTodo = document.createElement("li");
+        newTodo.innerText = todoInput.value; 
+        todoList.appendChild(newTodo);
+        const editButton = document.createElement("button");
+        editButton.innerText = "Edit";
+        editButton.addEventListener("click", editTodo);
+        newTodo.appendChild(editButton);
+        const deleteButton = document.createElement("button");
+        deleteButton.innerText = "Delete";
+        deleteButton.addEventListener("click", deleteTodo);
+        newTodo.appendChild(deleteButton);
+      }
+      todoInput.value = "";
+    }
+  
+    function editTodo() {
+      activeTodoItem = this.parentNode;
+      todoInput.value = activeTodoItem.firstChild.textContent;
+    }
+  
+    function deleteTodo() {
+      this.parentNode.remove();
+    }
+  
+    addButton.addEventListener("click", addTodo);
+  })();
